2b8908de94936d199407a2d355a890693e9541c6
Varunkumar Nagarajan <varunkumar.n@gmail.com>
2020-12-05 21:30:36 +0530

    Add schema for roomdata

---
+++ virtual-room/static/js/schema.js
@@ -0,0 +1,14 @@
+const schema = {
+  id: 'STRING',
+  users: [
+    {
+      userId: '',
+      active: true,
+      x: 0.5,
+      y: 0.5,
+      radius: 0.04,
+      alpha: 0.75,
+      clickable: true,
+    },
+  ],
+};
